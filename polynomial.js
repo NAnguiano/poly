4cc358f6c6ce2a7b470f4c8a304c8721726c2747
NAnguiano <nanguia1@lion.lmu.edu>
2017-01-24 14:06:31 -0800

    Add ESLint and format code for it.

--- polynomial.js
+++ polynomial.js
@@ -19,40 +19,50 @@ const grammar = ohm.grammar(`Polynomial {
 }`);
 
 const semantics = grammar.createSemantics().addOperation('deriv', {
-  Poly_add(p, op, t) {return `${p.deriv()}+${t.deriv()}`;},
-  Poly_subtract(p, op, t) {return `${p.deriv()}-${t.deriv()}`;},
-  Poly_negate(_, p) {return `-${p.deriv()}`;},
-  term_coeff_var_exp(c, _, e) {return `${e.value * c.value}x^${e.value - 1}`;},
-  term_coeff_var(c, _) {return `${c.value}`;},
-  term_coeff(c) {return `0`;},
-  term_var_exp(_, e) {return `${e.value}x^${e.value - 1}`;},
-  term_var(_) {return `1`;},
+  Poly_add(p, op, t) { return `${p.deriv()}+${t.deriv()}`; },
+  Poly_subtract(p, op, t) { return `${p.deriv()}-${t.deriv()}`; },
+  Poly_negate(_, p) { return `-${p.deriv()}`; },
+  term_coeff_var_exp(c, _, e) { return `${e.value * c.value}x^${e.value - 1}`; },
+  term_coeff_var(c, _) { return `${c.value}`; },
+  term_coeff(c) { return `0`; },
+  term_var_exp(_, e) { return `${e.value}x^${e.value - 1}`; },
+  term_var(_) { return `1`; },
 }).addOperation('eval', {
-  Poly_add(p, op, t) {return x => p.eval()(x) + t.eval()(x);},
-  Poly_subtract(p, op, t) {return x => p.eval()(x) - t.eval()(x);},
-  Poly_negate(_, p) {return x => -p.eval()(x);},
-  term_coeff_var_exp(c, _, e) {return x => c.value * x ** e.value;},
-  term_coeff_var(c, _) {return x => c.value * x;},
-  term_coeff(c) {return x => c.value;},
-  term_var_exp(_, e) {return x => x ** e.value;},
-  term_var(_) {return x => x;},
+  Poly_add(p, op, t) { return x => p.eval()(x) + t.eval()(x); },
+  Poly_subtract(p, op, t) { return x => p.eval()(x) - t.eval()(x); },
+  Poly_negate(_, p) { return x => -p.eval()(x); },
+  term_coeff_var_exp(c, _, e) { return x => c.value * x ** e.value; },
+  term_coeff_var(c, _) { return x => c.value * x; },
+  term_coeff(c) { return x => c.value; },
+  term_var_exp(_, e) { return x => x ** e.value; },
+  term_var(_) { return x => x; },
 }).addAttribute('value', {
-  coefficient(whole, dot, fraction) {return +this.sourceString;},
-  exponent(sign, magnitude) {return +this.sourceString;},
+  coefficient(whole, dot, fraction) { return +this.sourceString; },
+  exponent(sign, magnitude) { return +this.sourceString; },
 });
 
-exports.derivative = poly => {
-  let match = grammar.match(poly);
+exports.derivative = (poly) => {
+  const match = grammar.match(poly);
   if (!match.succeeded()) {
     throw new Error(match.message);
   }
   return semantics(match).deriv().replace(/--/g, '+').replace(/\+-/g, '-');
-}
+};
 
 exports.evaluate = (poly, x) => {
-  let match = grammar.match(poly);
+  const match = grammar.match(poly);
   if (!match.succeeded()) {
     throw new Error(match.message);
   }
   return semantics(match).eval()(x);
+};
+
+if (!module.parent) {
+  const match = grammar.math(process.argv[2]);
+  if (match.succeeded()) {
+    console.log(semantics(match).deriv());
+  } else {
+    console.error(match.message);
+    process.exitCode = 1;
+  }
 }
